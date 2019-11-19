23130fd561936dd902eab70343f96a6a24fd9223
Chanwut Kittivorawong <chanwutk@gmail.com>
2019-11-18 21:48:19 -0800

    clean up

--- src/visualizable_vgg.py
+++ src/visualizable_vgg.py
@@ -31,17 +31,22 @@ class VisualizableSequential(nn.Sequential):
     def get_visualizations(self):
         return self.visualizations
 
+    def set_visualizable(self, visualizable=True):
+        if visualizable != self.visualizations:
+            self.visualizable = visualizable
+            self.visualizations = [] if visualizable else None
+
 
 class VisualizableVgg(nn.Module):
     def __init__(self, save=True, visualizable=False, batch_norm=True, *args, **kwargs):
         self.visualizable = visualizable
         self.save = save
-        self.__best_accuracy
+        self.__best_accuracy_saved = None
         self.vgg = models.vgg19_bn()
         self.vgg.features = VisualizableSequential(self.vgg.features.layer)
 
-    def get_visualizations(self):
-        return self.vgg.features.get_visualizations()
+    def forward(self, input):
+        return self.vgg(input)
 
     def loss(self, prediction, label, reduction="elementwise_mean"):
         loss_val = F.cross_entropy(prediction, label.squeeze(), reduction=reduction)
@@ -51,8 +56,8 @@ class VisualizableVgg(nn.Module):
         pt_util.save(self, file_path, num_to_keep)
 
     def save_best_model(self, accuracy, file_path, num_to_keep=1):
-        if self.__best_accuracy is None or self.__best_accuracy < accuracy:
-            self.__best_accuracy = accuracy
+        if self.__best_accuracy_saved is None or self.__best_accuracy_saved < accuracy:
+            self.__best_accuracy_saved = accuracy
             self.save_model(file_path, num_to_keep)
 
     def load_model(self, file_path):
@@ -60,3 +65,11 @@ class VisualizableVgg(nn.Module):
 
     def load_last_model(self, dir_path):
         return pt_util.restore_latest(self, dir_path)
+
+    def set_visualizable(self, visualisable=True):
+        if visualisable != self.visualizable:
+            self.visualizable = visualisable
+            self.vgg.features.set_visualization(visualisable)
+
+    def get_visualizations(self):
+        return self.vgg.features.get_visualizations()
